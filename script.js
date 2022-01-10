e8f134f4660386de3e0a47c946380d86a290c177
Heather Graham <heather.graham@yahoo.com>
2022-01-10 10:09:00 -0700

    added more comments and fixed wording

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
+// Pass the password value into the box
 function generatePassword() {
   const passwordLength = getPasswordLength();
   const passwordCriteria = getPasswordCriteria();
@@ -17,16 +18,16 @@ function generatePassword() {
 // Ask the user to identify the input criteria for their password
 // Ensure that they select at least 1 from the criteria list
 function getPasswordCriteria() {
-  var useUppers = prompt("Would you like to use characters in your password? (Y/N");
-  var useLowers = prompt("Would you like to use characters in your password? (Y/N");
-  var useNumbers = prompt("Would you to use numbers in your password? (Y/N");
-  var useSymbols = prompt("Would you to use symbols in your password? (Y/N");
+  var useUppers = prompt("Use upper case characters in your password? (Y/N)");
+  var useLowers = prompt("Use lower case characters in your password? (Y/N)");
+  var useNumbers = prompt("Use numbers in your password? (Y/N)");
+  var useSymbols = prompt("Use symbols in your password? (Y/N)");
   while (useUppers.toUpperCase() !== "Y"  && useLowers.toUpperCase() !== "Y" && useNumbers.toUpperCase() !== "Y" & useSymbols.toUpperCase() !== "Y") {
     alert("You must select at least one criteria to generate a password");
-      useUppers = prompt("Would you like to use characters in your password?");
-      useLowers = prompt("Would you like to use characters in your password?");
-      useNumbers = prompt("Would you to use numbers in your password?");
-      useSymbols = prompt("Would you to use symbols in your password?");
+      useUppers = prompt("Use upper case characters in your password? (Y/N)");
+      useLowers = prompt("Use lower case characters in your password? (Y/N)");
+      useNumbers = prompt("Use numbers in your password? (Y/N)");
+      useSymbols = prompt("Use symbols in your password? (Y/N)");
   }
   return {
     "useUppers": useUppers,
@@ -43,7 +44,9 @@ const upperChar = "ABCDEFGHIJKLMNOPRSTUVWXYZ";
 const numbers = "1234567890";
 const symbols = "~!@#$%^&*()_+{}:?";
 
-// Based on the criteria the user selected, create a list of all characters to use in password
+// Based on the criteria the user selected, create a list of all characters to 
+// use in password generation
+
 function getPassword(passwordLength, passwordCriteria) {
   var chars = "";
   var buildPassword = "";
@@ -59,17 +62,19 @@ function getPassword(passwordLength, passwordCriteria) {
   if (passwordCriteria.useSymbols.toUpperCase() === "Y") {
     chars = chars+symbols;
   }
+  // build the password based on password length
   for (let index = 0; index < passwordLength; index++) {
     buildPassword += chars[Math.floor(Math.random() * chars.length)];
   }
   return buildPassword;
 }
+
 // Verify the password length is between 8 - 128 characters, otherwise ask again
 function getPasswordLength() {
-  var numDigits = prompt("Password length must be between 8 - 128");
+  var numDigits = prompt("Length of password (must be between 8 - 128 digits)");
   while (numDigits < 8 || numDigits > 128) {
    alert("Password must be between 8 and 128 digits");
-   numDigits = prompt("Password length must be between 8 - 128");
+   numDigits = prompt("Length of password (must be between 8 - 128 digits)");
   }
   return numDigits;
 }
